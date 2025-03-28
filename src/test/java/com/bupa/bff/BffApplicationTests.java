ef3cb17bfb9ecdf6452890ff1fe5ae58300ca65a
Jacek Feliksiak <jacfelik1@publicisgroupe.net>
2025-03-28 13:56:59 +0100

    Spring gradle java 21

---
+++ src/test/java/com/bupa/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupa.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
